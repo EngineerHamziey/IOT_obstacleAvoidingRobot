92589c3ae5de6cd5798faa89db8a54df1bf45663
EngineerHamziey <engineerlawalhamzatademola@gmail.com>
2024-03-24 13:14:39 +0100

    all wheels and remote control working perfectly

--- IOT_obstacleAvoidingRobot.ino
+++ IOT_obstacleAvoidingRobot.ino
@@ -1,6 +1,5 @@
-void beginTheSerialMonitor() { Serial.begin(115200);}
-
-#if 1
+void beginTheSerialMonitor() {Serial.begin(115200);}
+#if 0
 
 //Add software serial library so that you can add custom UART, tx, rx
 #include <SoftwareSerial.h>
@@ -10,10 +9,10 @@ SoftwareSerial btModule(btTx, btRx);
 
 //back Wheels
 const int
-  back_rightMotorForward = 10,
-  back_rightMotorBackward = 11,
-  back_leftMotorForward = 13,
-  back_leftMotorBackward = 12;
+  back_rightMotorForward = 13,
+  back_rightMotorBackward = 12,
+  back_leftMotorForward = 10,
+  back_leftMotorBackward = 11;
 
 //front Wheels
 const int
@@ -21,7 +20,6 @@ const int
   front_rightMotorBackward = 0,
   front_leftMotorForward = 8,
   front_leftMotorBackward = 7;
-
 
 const int trigPin = A3;
 const int echoPin = A1;
@@ -40,7 +38,7 @@ int calculateDistance() {//calculate distance in centimeter
   //store the time taken for echo to return in a variable
   echoReturnTime = pulseIn(echoPin, HIGH);
   calculatedDistance = echoReturnTime / 58.2;//calculate distance using speed of sound which is 343m/s approxe
-  delay(1000);  // Adjust delay as needed
+  delay(500);  // Adjust delay as needed
 
   return calculatedDistance;
 }
@@ -65,69 +63,69 @@ void setPinMode() {
 }
 
 void goForward() {  //motor right and left
-  //right wheel, go forward
-  digitalWrite(front_rightMotorForward, HIGH);  //GIVE THIS PIN +5V
+  //right wheel, go forward//////////////////--------------------------------WHEEL NOT WORKING
+  digitalWrite(front_rightMotorForward, HIGH);  //GIVE THIS PIN +5V 
   digitalWrite(front_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, go forward
+  //left wheel, go forward  --------------------------------WORKING
   digitalWrite(front_leftMotorForward, HIGH);  //GIVE THIS PIN +5V
   digitalWrite(front_leftMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
 
   
-  //right wheel, go forward
+  //right wheel, go forward----------------------------------------------------WHEEL NOT WORKING
   digitalWrite(back_rightMotorForward, HIGH);  //GIVE THIS PIN +5V
   digitalWrite(back_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, go forward
+  //left wheel, go forward-----------------------------------WORKING
   digitalWrite(back_leftMotorForward, HIGH);  //GIVE THIS PIN +5V
   digitalWrite(back_leftMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
 }
 
 void goBackward() {  //motor right and left
-  //right wheel, go BACKWARD
+  //right wheel, go BACKWARD----------------------------------NOT WORKING
   digitalWrite(front_rightMotorForward, LOW);
   digitalWrite(front_rightMotorBackward, HIGH);
-  //left wheel, go BACKWARD
+  //left wheel, go BACKWARD-------------------------WORKING
   digitalWrite(front_leftMotorForward, LOW);
   digitalWrite(front_leftMotorBackward, HIGH);
 
   
-  //right wheel, go BACKWARD
+  // right wheel, go BACKWARD-------------------------- WORKING
   digitalWrite(back_rightMotorForward, LOW);
   digitalWrite(back_rightMotorBackward, HIGH);
-  //left wheel, go BACKWARD
+  // left wheel, go BACKWARD-----------------------------------WORKING
   digitalWrite(back_leftMotorForward, LOW);
   digitalWrite(back_leftMotorBackward, HIGH);
 }
 
 void goLeft() {  //RIGHT == FORWARD, LEFT Stop
-  //right wheel, go forward
-  digitalWrite(front_rightMotorForward, HIGH);  //GIVE THIS PIN +5V
+  //right wheel, go BACKWARD
+  digitalWrite(front_rightMotorForward, HIGH);   //GIVE THIS PIN +5V
   digitalWrite(front_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, stay still
+  //left wheel, GO FORWARD
   digitalWrite(front_leftMotorForward, LOW);
-  digitalWrite(front_leftMotorBackward, LOW);
+  digitalWrite(front_leftMotorBackward, HIGH);
 
   
-  //right wheel, go forward
-  digitalWrite(back_rightMotorForward, HIGH);  //GIVE THIS PIN +5V
+  //right wheel, go BACKWARD
+  digitalWrite(back_rightMotorForward, HIGH);   //GIVE THIS PIN +5V
   digitalWrite(back_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, stay still
+  //left wheel, GO FORWARD
   digitalWrite(back_leftMotorForward, LOW);
-  digitalWrite(back_leftMotorBackward, LOW);
+  digitalWrite(back_leftMotorBackward, HIGH);
 }
 
 void goRight() {  //RIGHT == stop, LEFT = forward
   //right wheel, go forward
   digitalWrite(front_rightMotorForward, LOW);   //GIVE THIS PIN +5V
-  digitalWrite(front_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, stay still
+  digitalWrite(front_rightMotorBackward, HIGH);  //GIVE THIS PIN 0(GND) 0
+  //left wheel, GO BACKWARD
   digitalWrite(front_leftMotorForward, HIGH);
   digitalWrite(front_leftMotorBackward, LOW);
 
   
   //right wheel, go forward
   digitalWrite(back_rightMotorForward, LOW);   //GIVE THIS PIN +5V
-  digitalWrite(back_rightMotorBackward, LOW);  //GIVE THIS PIN 0(GND) 0
-  //left wheel, stay still
+  digitalWrite(back_rightMotorBackward, HIGH);  //GIVE THIS PIN 0(GND) 0
+  //left wheel, GO BACKWARD
   digitalWrite(back_leftMotorForward, HIGH);
   digitalWrite(back_leftMotorBackward, LOW);
 }
@@ -202,22 +200,18 @@ void IOTDrive(char command) {
 }
 
 void obstacleAvoidanceMode() {
-    int distance = calculateDistance();
+    const int turnDelay = 3;// seconds
 
-    if (distance < 10) {
+    if (calculateDistance() < 10) {
         // Obstacle detected, perform avoidance maneuver
         stopMoving();  // Stop the car
-        
         servoRotateRight();
         int rightSideDistance = calculateDistance();
         servoRotateLeft();
-        int leftSideDistance = calculateDistance;
-
-        if (leftSideDistance > rightSideDistance) {
-            goLeft();  
-        } else {
-            goRight();  
-        }
+        int leftSideDistance = calculateDistance();
+        servoRotateFront();//return to normal postion
+        (leftSideDistance > rightSideDistance) ? goLeft() : goRight();
+        delay(turnDelay * 1000);
     } else {
         // No obstacle, continue normal operation
         goForward();
@@ -225,34 +219,43 @@ void obstacleAvoidanceMode() {
 }
 
 void testAllMovements() {
-  int moveDelay = 2000;
-  goForward();
-  delay(moveDelay);
-  goBackward();
-  delay(moveDelay);
-  goLeft();
-  delay(moveDelay);
-  goRight();
-  delay(moveDelay);
+  // RIGHT, LEFT, ETC ARE DETERMINED WHRN THE CAR FACES ME
+  // //right wheel, go forward//////////////////-------------------------------- WORKING
+  // digitalWrite(front_rightMotorForward, HIGH);  // FORWARD WORKING
+  // digitalWrite(front_rightMotorBackward, LOW);  // BACKWARD WORKING
+
+  //left wheel, go forward  --------------------------------WORKING
+  // digitalWrite(front_leftMotorForward, HIGH);  //FORWARD WORING
+  // digitalWrite(front_leftMotorBackward, LOW);  //BACKWARD WORKING
+
+  
+  //right wheel, go forward----------------------------------------------------WHEEL NOT WORKING
+  // digitalWrite(back_rightMotorForward, LOW);  // FORWARD WORKING
+  // digitalWrite(back_rightMotorBackward, HIGH);  //BACKWARD WORKING
+
+  //left wheel, go forward-----------------------------------WORKING
+  // digitalWrite(back_leftMotorForward, HIGH);  // FORWARD WORKING
+  // digitalWrite(back_leftMotorBackward, LOW);  //BACKWARD WORKING
+
 }
 
 
 void setup() {
   btModule.begin(9600);
-  beginTheSerialMonitor();
   setPinMode();
   stopMoving();
 }
 
-const char
-    autoDrive = 'd';
-
-
+const char autoDrive = 'd';
 int moveDelay = 2000;
 void loop() {
+  // testAllMovements();
+  // goForward();
+  // goRight();
+  // goLeft();
+
   if (btModule.available() > 0) {
     char receivedInstruction = btModule.read(); // read the incoming command
-    Serial.println("received char = " + String(receivedInstruction));
     switch(receivedInstruction) {
       case autoDrive:
         obstacleAvoidanceMode();
@@ -262,10 +265,7 @@ void loop() {
         break;
     }
   }
-// */
 
 }
 
 #endif
-
-//========================================
--- ultrasonic_sensor_test.ino
+++ ultrasonic_sensor_test.ino
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 
 const int trigPin = A3;
 const int echoPin = A1;
@@ -17,9 +17,33 @@ int calculateDistance() {//calculate distance in centimeter
   //store the time taken for echo to return in a variable
   echoReturnTime = pulseIn(echoPin, HIGH);
   calculatedDistance = echoReturnTime / 58.2;//calculate distance using speed of sound which is 343m/s approxe
-  delay(1000);  // Adjust delay as needed
+  delay(500);  // Adjust delay as needed
 
   return calculatedDistance;
+}
+
+void obstacleAvoidanceMode() {
+  const int turnDelay = 3;// seconds
+
+  if (calculateDistance() < 10) {
+    Serial.println("distance less than threshold");
+    // stopMoving();  // Stop the car
+    // Obstacle detected, perform avoidance maneuver
+    // servoRotateRight();
+    int rightSideDistance = calculateDistance();
+    // servoRotateLeft();
+    int leftSideDistance = calculateDistance();
+    // servoRotateFront();//return to normal postion
+    (leftSideDistance > rightSideDistance) ? Serial.println("go left") : Serial.println("go right");
+    
+
+
+    delay(turnDelay * 1000);
+  } else {
+      // No obstacle, continue normal operation
+      // goForward();
+      Serial.println("no obstacle, go forward");
+  }
 }
 
 void setup() {
@@ -32,6 +56,7 @@ void loop() {
   Serial.print("Distance: ");
   Serial.print(calculateDistance());
   Serial.println(" cm");
+  obstacleAvoidanceMode();
 }
 
 #endif
